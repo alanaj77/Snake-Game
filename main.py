2302e6db26dcecde4f164e5e95d9494f56924302
Alan Joseph <alanjoseph52006@gmail.com>
2026-06-07 02:12:16 +0530

    Add food collision logic and snake growth in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle,Screen
+from turtle import Screen
 import time
+import  random
 from snake import Snake
+from food  import Food
 screen = Screen()
 screen.tracer(0)
 screen.setup(height = 600,width = 600)
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.listen()
 
 snake = Snake()
+food = Food()
+
 
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -20,4 +24,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.goto(random.randint(-280, 280), random.randint(-280, 280))
+        snake.create_segment()
+
+
+
+
 screen.exitonclick()
