35274c34c39e4f001dd661069513fbb00a650a6e
Alan Joseph <alanjoseph52006@gmail.com>
2026-06-13 09:42:24 +0530

    Updated score.py to track the higest score

--- score.py
+++ score.py
@@ -1,22 +1,41 @@
 from turtle import Turtle
+
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score_val = 0
         self.penup()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
-        self.goto(0,280)
+        self.goto(0,250)
         self.color("white")
-        self.write("Score = ",True,"center")
-        self.write( self.score_val, True)
+        self.goto(0, 275)
+        self.write("High Score = ", True, "center",font=("arial",15,"bold"))
+        with open("data.txt") as data:
+            high_score = data.read()
+            self.write(high_score, True,font=("arial",15,"bold"))
     def update_score(self):
         self.clear()
         self.score_val += 1
-        self.goto(0, 280)
-        self.write("Score = ",True,"center")
-        self.write( self.score_val, True)
+        self.goto(0, 250)
+
+        self.write("Score = ",True,"center",font=("arial",15,"bold"))
+        self.write( self.score_val, True,font=("arial",15,"bold"))
+
 
     def game_over(self):
-        self.clear()
+        print(f"{self.high_score},{self.score_val}")
+        if self.high_score < self.score_val:
+            self.high_score = self.score_val
+            print(self.high_score)
+            with open("data.txt",mode = "w+") as data:
+
+                print("hello")
+                high_score = str(self.high_score)
+                data.write(high_score)
+
+        self.goto(0,0)
         self.color("White")
-        self.write("Game Over", True, "center")
+        self.write("Game Over", True, "center",font=("arial",30,"bold"))
